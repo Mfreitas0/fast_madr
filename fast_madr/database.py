81ebfc9be7e439ed4b09d3f80514f3bac5ea03c9
Mfreitas0 <mfreitasc0@gmail.com>
2024-09-21 08:06:13 -0300

    Configurando o database e vinculando ao endpoints.

---
+++ fast_madr/database.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Mapped, mapped_column, registry, sessionmaker
+from sqlalchemy import create_engine
+
+
+reg = registry()
+
+engine = create_engine(
+    "postgresql+psycopg://app_user:app_password@localhost:5433/app_madr",
+)
+
+
+@reg.mapped_as_dataclass
+class User:
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+
+
+Session = sessionmaker(engine)
+reg.metadata.create_all(engine)
+
+
+# funcao de controle da sessao
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
--- fast_madr/main.py
+++ fast_madr/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from .database import User, get_db
 from .model import UserModel
 
 
@@ -11,24 +14,49 @@ def read_rot():
 
 
 @app.get("/users/")
-def read_users():
-    return {}
+def read_users(db: Session = Depends(get_db)):
+    q = db.query(User).order_by(User.id).all()
+    return q
 
 
 @app.post("/user/")
-def create_user(user: UserModel):
-    return {user.username, user.email, user.password}
+def create_user(user: UserModel, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter((User.email == user.email)).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User already exists.")
+    new_user = User(username=user.username, email=user.email, password=user.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"username": user.username, "emal": user.email, "password": user.password}
 
 
 @app.put("/user/{user_id}")
-def update_user(user_id: int, user: UserModel):
-    if not user_id:
+def update_user(user_id: int, user: UserModel, db: Session = Depends(get_db)):
+    up_user = db.get(User, user_id)
+    if not up_user:
         raise HTTPException(status_code=404, detail="User not found.")
-    return {"username": user.username, "email": user.email, "password": user.password}
+    else:
+        up_user.username = user.username
+        up_user.email = user.email
+        up_user.password = user.password
+    db.commit()
+    db.refresh(up_user)
+
+    return {
+        "id": user_id,
+        "username": user.username,
+        "email": user.email,
+        "password": user.password,
+    }
 
 
 @app.delete("/user/{user_id}")
-def delete_user(user_id: int, user: UserModel):
-    if not user_id:
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    existing_user = db.get(User, user_id)
+    if not existing_user:
         raise HTTPException(status_code=404, detail="User not found.")
+    db.delete(existing_user)
+    db.commit()
     return {"detail": "User deleted"}
